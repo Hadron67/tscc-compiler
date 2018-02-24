b994bbca4346711b72c0e4160108cc156e72cb6a
Hadron67 <604700341@qq.com>
2018-02-24 23:28:06 +0800

    small fixings

--- web-demo/src/tscc-highlight-codemirror.js
+++ web-demo/src/tscc-highlight-codemirror.js
@@ -2,14 +2,6 @@ void function(CodeMirror){
     'use strict';
     var stringPattern = /"([^"\n\r]|\\([nrbf"]|[uUxX][0-9a-fA-F]+))*"|'([^'\n\r]|\\([nrbf']|[uUxX][0-9a-fA-F]+))*'/;
     var idPattern = /[a-zA-Z$_][a-zA-Z0-9$_]*/;
-    CodeMirror.defineMode('tscc', {
-        start: [
-            { regexp: /%(left|right|nonassoc|option|token|output|import|least)/, token: 'keyword' },
-            { regexp: stringPattern, token: 'string' },
-            { regexp: idPattern, token: 'variable' }
-        ],
-        block: [],
-        actionBlock: [],
-        comment: []
-    });
+    
+    
 }(CodeMirror);
